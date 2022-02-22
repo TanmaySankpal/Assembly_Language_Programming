e30c9d74f033c25fdc975a261c953b20e396b674
Tanmay Sankpal <tanmaysankpal119@gmail.com>
2022-02-22 23:12:48 +0530

    Add files via upload

---
+++ Branching_and_Looping/if_1.c
@@ -0,0 +1,16 @@
+#include <stdio.h> 
+#include <stdlib.h> 
+
+int n1 = 10; 
+int n2 = 5; 
+
+int main(){
+	puts("U:This is a start of the program"); 
+	
+	if(n1 > n2){
+		puts("C:This will be printed if n1 is greater than n2"); 
+	}
+
+	puts("U:This is a last statement of program"); 
+	exit(0); 
+}
