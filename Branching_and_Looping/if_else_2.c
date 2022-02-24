e848d09c922da1f278d03bf3815fdc88fdb2bf82
Tanmay Sankpal <tanmaysankpal119@gmail.com>
2022-02-24 21:44:49 +0530

    Add files via upload

---
+++ Branching_and_Looping/if_else_2.c
@@ -0,0 +1,19 @@
+#include <stdio.h> 
+#include <stdlib.h> 
+
+int a = 100; 
+int b = 50; 
+int c = 20; 
+int d = 80; 
+
+int main(){
+	
+	if(a > b && c < d){
+		puts("True"); 
+	}else{
+		puts("False"); 
+	}
+	
+	exit(0); 
+
+}
