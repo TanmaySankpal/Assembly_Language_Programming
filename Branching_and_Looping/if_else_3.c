30f278b884379f3d8ef099d71aaa6e03ae062063
Tanmay Sankpal <tanmaysankpal119@gmail.com>
2022-02-25 22:16:38 +0530

    Add files via upload

---
+++ Branching_and_Looping/if_else_3.c
@@ -0,0 +1,28 @@
+#include <stdio.h> 
+#include <stdlib.h> 
+
+int a, b; 
+unsigned int c, d; 
+
+int main(){
+
+	printf("Enter a:");
+	scanf("%d", &a); 
+
+	printf("Enter b:"); 
+	scanf("%d", &b); 
+
+	printf("Enter c:"); 
+	scanf("%u", &c); 
+
+	printf("Enter d:"); 
+	scanf("%u", &d); 
+
+	if(a > b || c < d){
+		puts("True"); 
+	}else{
+		puts("False"); 
+	}
+
+	exit(0); 
+}
